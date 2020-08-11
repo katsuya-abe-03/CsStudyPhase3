7e2a59d55b73c46d1811314a29acee4f08c5ad46
katsuya-abe-03 <katsuya_abe@sobal.co.jp>
2020-08-11 10:13:11 +0900

    実装完了

---
+++ Phase3App/ColorConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Phase3App
+{
+    public class ColorConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            byte R = (byte)(double)(values[0]);
+            byte G = (byte)(double)(values[1]);
+            byte B = (byte)(double)(values[2]);
+
+            return Color.FromRgb(R, G, B);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            //片方向のバインディングなので、今回は未サポート
+            return null;
+//            throw new NotSupportedException();
+        }
+    }
+}
---
+++ Phase3App/SolidColorBrushConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Phase3App
+{
+    public class SolidColorBrushConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            //FillはSolidColorBrush型
+            SolidColorBrush brush = new SolidColorBrush();
+
+            //R,G,Bの順にバインディングされているので、そのままの順番で使用する
+            brush.Color = Color.FromArgb(255, (byte)(double)values[0], (byte)(double)values[1], (byte)(double)values[2]);
+
+            return brush;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            //片方向のバインディングなので、今回は未サポート
+            return null;
+//            throw new NotSupportedException();
+        }
+    }
+}
